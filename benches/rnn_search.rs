ef7faca36e714880ad79f492308d6503af817a92
Najib Ishaq <najib_ishaq@zoho.com>
2022-08-27 15:08:14 -0400

    wip: baseline benchmark for current rnn implementation

--- benches/rnn_search.rs
+++ benches/rnn_search.rs
@@ -7,37 +7,42 @@ use criterion::Criterion;
 
 use clam::prelude::*;
 
-use utils::anomaly_readers;
+use utils::search_readers;
 
 fn cakes(c: &mut Criterion) {
     let mut group = c.benchmark_group("rnn-search");
     group
         .significance_level(0.05)
-        // .measurement_time(std::time::Duration::new(60, 0)); // 60 seconds
-        .sample_size(100);
+        .measurement_time(std::time::Duration::new(10, 0)) // 10 seconds
+        .sample_size(30);
 
-    for &data_name in anomaly_readers::ANOMALY_DATASETS.iter() {
-        let (features, _) = anomaly_readers::read_anomaly_data(data_name, true).unwrap();
+    for &(data_name, metric_name) in search_readers::SEARCH_DATASETS.iter() {
+        if !data_name.contains("mnist") {
+            continue;
+        }
 
-        let dataset = clam::Tabular::new(&features, data_name.to_string());
-        let queries = (0..1000)
-            .map(|i| dataset.get(i % dataset.cardinality()))
+        let (train, test) = search_readers::read_search_data(data_name).unwrap();
+
+        let train = clam::Tabular::new(&train, data_name.to_string());
+        let test = clam::Tabular::new(&test, data_name.to_string());
+
+        let queries = (0..100)
+            .map(|i| test.get(i % test.cardinality()))
             .collect::<Vec<_>>();
 
-        let metric_name = "euclidean";
         let metric = metric_from_name::<f32, f32>(metric_name, false).unwrap();
-        let space = clam::TabularSpace::new(&dataset, metric.as_ref(), false);
+        let space = clam::TabularSpace::new(&train, metric.as_ref(), false);
         let partition_criteria = clam::PartitionCriteria::default();
         let cakes = clam::CAKES::new(&space).build(&partition_criteria);
 
         let radius = cakes.radius();
-        let radii_factors = (50..100).step_by(50);
+        let radii_factors = (5..25).step_by(5).chain((25..=100).step_by(25)).collect::<Vec<_>>();
 
         let bench_name = format!(
             "{}-{}-{}-{}",
             data_name,
-            dataset.cardinality(),
-            dataset.dimensionality(),
+            train.cardinality(),
+            train.dimensionality(),
             metric_name
         );
         for factor in radii_factors {
